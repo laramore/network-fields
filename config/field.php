e29d7950bc41e68189e0ec3bb7514bdb3697f87f
NastuzziSamy <samy@nastuzzi.fr>
2020-05-15 20:22:06 +0200

    Define configs for url, ip and ftp

---
+++ config/field.php
@@ -0,0 +1,64 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Default text fields
+    |--------------------------------------------------------------------------
+    |
+    | This option defines the default text fields.
+    |
+    */
+
+    'configurations' => [
+        'ipv4' => [
+            'type' => 'ipv4',
+            'max_length' => 15,
+            'pattern' => '/^(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/',
+        ],
+        'ipv6' => [
+            'type' => 'ipv6',
+            'max_length' => 45, // https://stackoverflow.com/a/7477384/13550768 .
+            'pattern' => '/^(((?=(?>.*?(::))(?!.+\3)))\3?|([\dA-F]{1,4}(\3|:(?!$)|$)|\2))(?4){5}((?4){2}|((2[0-4]|1\d|[1-9])?\d|25[0-5])(\.(?7)){3})\z/i',
+        ],
+    'url' => [
+        'type' => 'url',
+        'max_length' => Schema::getFacadeRoot()::$defaultStringLength,
+        'secured' => false,
+        'proxy' => [
+            'configurations' => [
+                'fix' => [],
+            ],
+        ],
+        'patterns' => [
+            'identifier' => '/^\S+$/',
+            'protocol' => '/^https?:\/\/$/',
+            'secured_protocol' => '/^https:\/\/$/',
+            'uri' => '/^https?:\/\/\S+$/',
+            'secured_uri' => '/^https?:\/\/\S+$/',
+            'flags' => null,
+        ]
+    ],
+    'ftp' => [
+        'type' => 'ftp',
+        'max_length' => Schema::getFacadeRoot()::$defaultStringLength,
+        'secured' => false,
+        'proxy' => [
+            'configurations' => [
+                'fix' => [],
+            ],
+        ],
+        'patterns' => [
+            'identifier' => '/^([a-z0-9]+:[a-z0-9]+@)?([\.a-z0-9]+)/([\./a-z0-9]+)$/',
+            'protocol' => '/^ftp:\/\/$/',
+            'uri' => '/^ftp:\/\/([a-z0-9]+:[a-z0-9]+@)?([\.a-z0-9]+)/([\./a-z0-9]+)$/',
+            'flags' => null,
+        ]
+    ],
+    ],
+    
+];
---
+++ config/type.php
@@ -0,0 +1,23 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Default text types
+    |--------------------------------------------------------------------------
+    |
+    | This option defines the default text types used by fields.
+    |
+    */
+
+    'configurations' => [
+        'url' => [
+            'native' => 'url',
+            'default_options' => [
+                'visible', 'fillable', 'required',
+            ],
+        ],
+    ],
+
+];
